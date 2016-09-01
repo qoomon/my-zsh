4ee16730ffddd5f5a51930c596b8efdfbdaaccdb
Bengt Brodersen <bengt.brodersen@deposit-solutions.com>
2016-09-01 15:05:47 +0200

    no message

--- modules/random.zsh
+++ modules/random.zsh
@@ -2,3 +2,13 @@ function random_character {
   local character_count=${1:-32}
   cat /dev/urandom | base64 | head -c $character_count
 }
+
+function random_number {
+  local number_count=${1:-32}
+  local number=''
+  for index in $(seq 1 $number_count); do 
+    local random=$RANDOM
+    number="${number}$(expr $random % 10 )"
+  done
+  echo $number
+}
