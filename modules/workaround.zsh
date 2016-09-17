537efc569042185e815cfaad18b753d51a741a53
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-09-18 00:12:24 +0200

    no message

--- modules/workaround.zsh
+++
@@ -1,7 +0,0 @@
-# WORKAROUND user zsh completion for z
-function __z {
-  local directories=($(_z 2>&1 | sed 's|^[^/]*||'))
-  _describe 'visited directory' directories
-  return 0
-}
-compdef __z _z
