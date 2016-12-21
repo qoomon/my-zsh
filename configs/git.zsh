086d2818771a11c998ec8cdb820575a72204ffd3
Bengt Brodersen <bengt.brodersen@gmail.com>
2016-12-21 12:39:11 +0100

    .

--- configs/git.zsh
+++ configs/git.zsh
@@ -4,7 +4,10 @@ git config --global pull.rebase true
 git config --global rebase.autoStash true
 git config --global push.followTags true
 git config --global tag.sort version:refname
-# git config --global credential.helper osxkeychain
+# git config --global commit.template ~/git_commit_template.txt
+if [ "$(uname)" == "Darwin" ]; then
+  git config --global credential.helper osxkeychain
+fi
 
 # list all alias
 git config --global alias.alias '\!git config --get-regexp '\''alias'\'' | sed -E -e '\''s|^alias\.||'\'' | grep -v -e '\''^alias'\'' | sort | sed -E -e '\''s|^([^ ]*)( .*)|${fg_bold[blue]}\1###${fg[white]}\2$reset_color|'\'' | column -s '\''###'\'' -t'
@@ -15,9 +18,9 @@ git config --global alias.logx 'log --graph --all --date=format:'\''%a %Y-%m-%d 
 # get commit hash for HEAD by default or Branch or Tag
 git config --global alias.hash '!sh -c '\''git rev-parse ${1:-HEAD}'\'' -'
 
-if type atom >/dev/null; then
-  git config --global core.editor 'atom --wait'
-fi
+# if type atom >/dev/null; then
+#   git config --global core.editor 'atom --wait'
+# fi
 
 if type idea >/dev/null; then
   git config --global mergetool.intellij.cmd 'idea merge $(cd $(dirname "$LOCAL") && pwd)/$(basename "$LOCAL") $(cd $(dirname "$REMOTE") && pwd)/$(basename "$REMOTE") $(cd $(dirname "$BASE") && pwd)/$(basename "$BASE") $(cd $(dirname "$MERGED") && pwd)/$(basename "$MERGED")'
@@ -29,16 +32,16 @@ if type idea >/dev/null; then
   git config --global diff.tool intellij
 fi
 
-# find template in files folder
-# git config --global commit.template ~/git_commit_template.txt
 
+## seach for changes
+# git log --follow -G'<SEARCH_STRING>' --patch [ -- <FILE_PATH> ]
+## show file in spesific version
+# git show <COMMIT>:<FILE_PATH>
 
 # npm install --global commitizen
 # npm install --global cz-conventional-changelog
 # echo '{ "path": "cz-conventional-changelog" }' > ~/.czrc
-
 # npm install --global conventional-changelog-cli
-
 # npm install --global conventional-recommended-bump
 
 #################
