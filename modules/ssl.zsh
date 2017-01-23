27472165825cdce2e43adaa0d16d2aad030256cd
Bengt Brodersen <bengt.brodersen@gmail.com>
2017-01-23 09:17:12 +0100

    no message

---
+++ modules/ssl.zsh
@@ -0,0 +1,8 @@
+function openssl_download_certificate {
+  local host=$1
+  local port=${2:-443}
+  openssl s_client -showcerts -connect "${host}:${port}" </dev/null 2>/dev/null | openssl 'x509' -outform 'PEM' > "${host}:${port}.pem"
+}
+
+# generate fingerprint from certificate
+#openssl x509 -in certificate.crt -pubkey -noout | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64
