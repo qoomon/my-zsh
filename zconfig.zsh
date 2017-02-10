b79cf9eb04ddf0fcd3f83e1bfe17b6449b641c5a
Bengt Brodersen <bengt.brodersen@gmail.com>
2017-02-10 17:30:31 +0100

    no message

--- zconfig.zsh
+++ zconfig.zsh
@@ -8,28 +8,26 @@ if zprofile::active; then zprofile::before; fi
 # ZGEM_VERBOSE='true'
 source "$SELF_DIR/plugins/zgem.zsh"
 
-zgem add "$SELF_DIR/modules/general.zsh"
-
-zgem add "$SELF_DIR/modules/prompt.zsh"
-
-zgem add "$SELF_DIR/modules/history.zsh"
-
-zgem add "$SELF_DIR/modules/alias.zsh"
+zgem add 'https://github.com/rupa/z.git' from:'git' use:'z.sh'
 
 zgem add 'https://github.com/zsh-users/zsh-syntax-highlighting.git' from:'git' use:'zsh-syntax-highlighting.zsh'
-zgem add 'https://github.com/zsh-users/zsh-history-substring-search.git' from:'git' use:'zsh-history-substring-search.zsh'
-HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=default,fg=magenta,bold'
-HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=default,fg=black,bold'
-HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS='i'
-bindkey '^[[A' history-substring-search-up     # bind arrow-up
-bindkey '^[[B' history-substring-search-down   # bind arrow-down
-
-zgem add 'https://github.com/rupa/z.git' from:'git' use:'z.sh'
+zgem add 'https://github.com/zsh-users/zsh-history-substring-search.git' from:'git' use:'zsh-history-substring-search.zsh' && { 
+  HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=default,fg=magenta,bold'
+  HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=default,fg=black,bold'
+  HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS='i'
+  bindkey '^[[A' history-substring-search-up     # bind arrow-up
+  bindkey '^[[B' history-substring-search-down   # bind arrow-down
+}
 
 zgem add 'https://github.com/zsh-users/zsh-completions.git' from:'git' use:'zsh-completions.plugin.zsh'
 zgem add 'https://raw.githubusercontent.com/docker/docker/master/contrib/completion/zsh/_docker' from:'http' as:'completion'
 zgem add 'https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose' from:'http' as:'completion'
+
+zgem add "$SELF_DIR/modules/general.zsh"
+zgem add "$SELF_DIR/modules/history.zsh"
+zgem add "$SELF_DIR/modules/prompt.zsh"
 zgem add "$SELF_DIR/modules/completion.zsh"
+zgem add "$SELF_DIR/modules/alias.zsh"
 
 zgem add "$SELF_DIR/utils/color.zsh"
 zgem add "$SELF_DIR/utils/which.zsh"
@@ -41,10 +39,9 @@ zgem add "$SELF_DIR/utils/process.zsh"
 zgem add "$SELF_DIR/utils/ssh.zsh"
 zgem add "$SELF_DIR/utils/git.zsh"
 zgem add "$SELF_DIR/utils/docker.zsh"
-
 zgem add "$SELF_DIR/utils/http-server.zsh"
-zgem add "$SELF_DIR/utils/maven.zsh"
 
+zgem add "$SELF_DIR/utils/maven.zsh"
 zgem add "$SELF_DIR/utils/osx.zsh"
 zgem add "$SELF_DIR/utils/pane.zsh"
 
