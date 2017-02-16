d392730ed7e4f85b3a2582de1ea442d60516c07c
Bengt Brodersen <bengt.brodersen@gmail.com>
2017-02-16 14:23:47 +0100

    no message

--- install.zsh
+++ install.zsh
@@ -1,4 +1,6 @@
 #!/bin/zsh
+autoload +X -U colors && colors
+
 function ask {
   read -p "$1 " response
   [[ $response == "y" || $response == "Y" || $response == "yes" || $response == "Yes" ]]
@@ -13,19 +15,19 @@ ZSHCONF_DIR="$(echo "$SELF_DIR" | sed "s|^$HOME|\${HOME}|")"
 ZSHRC_FILE_COMMAND="test -e \"$ZSHCONF_DIR/zconfig.zsh\" && source \"$ZSHCONF_DIR/zconfig.zsh\""
 
 if grep -xq "$ZSHRC_FILE_COMMAND" "$ZSHRC_FILE" >/dev/null; then
-  echo "zsh config already installed within $ZSHRC_FILE"
+  echo "zconfig already installed to $ZSHRC_FILE"
 else
-  echo "install zsh config within $ZSHRC_FILE"
-  echo "$ZSHRC_FILE_COMMAND" >> "$ZSHRC_FILE"
+  echo "zconfig install to $ZSHRC_FILE"
+  {echo "$ZSHRC_FILE_COMMAND\n"; cat "$ZSHRC_FILE"} | tee "$ZSHRC_FILE" >/dev/null;
 fi
 
-USER=${USER:-$(id -un)}
-if [ "$(uname)" == "Darwin" ]; then
+USER=${USER:-$(whoami)}
+if [ "$(uname)" = "Darwin" ]; then
   CURRENT_USER_SHELL=$(dscl . -read /Users/$USER | grep UserShell | cut -d' ' -f2)
 else
   CURRENT_USER_SHELL=$(getent passwd $USER | cut -d':' -f7)
 fi
-if [ "$CURRENT_USER_SHELL" == "/bin/zsh" ]; then
+if [ "$CURRENT_USER_SHELL" = "/bin/zsh" ]; then
   echo "user shell already is /bin/zsh"
 elif ask "Want to change shell for current user?"; then
   if ! grep -xq "/bin/zsh" "/etc/shells"; then
