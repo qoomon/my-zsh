f455b8e09698b81dd2269f10216b0a19fcc742e7
Bengt Brodersen <bengt.brodersen@gmail.com>
2017-03-13 07:29:19 +0100

    no message

--- utils/domain.zsh
+++ utils/domain.zsh
@@ -2,9 +2,9 @@ function checkdomain {
   local sld=$1
   local tdl_list=($(curl -s http://data.iana.org/TLD/tlds-alpha-by-domain.txt  | grep -v '#\|\-\-' | awk '{ print length($0) " " $0; }' | sort -n | cut -d ' ' -f 2-))
   for tld in $tdl_list; do
-    if dig $sld.$tld +noquestion +nostat +noanswer +noauthority | grep "ANSWER: 0" >/dev/null; then
-      echo $sld.$tld
+    local answer=$(dig @8.8.8.8 $sld.$tld +noquestion +nostat +noanswer +noauthority | grep "ANSWER: 0")
+    if [ -n "$answer" ]; then
+      echo "$sld.$tld"
     fi
-    echo "done"
   done
 }
