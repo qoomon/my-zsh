2827287e85b5e7ba8a203a0804761517f60a3df6
Bengt Brodersen <bengt.brodersen@gmail.com>
2017-10-22 01:01:44 +0200

    minor

--- modules/alias.zsh
+++ modules/alias.zsh
@@ -37,6 +37,8 @@ alias http-server='command http-server -p 8080 -o'
 alias http-server-ssl="command http-server -p 8443 -o --ssl --cert $ZSH_FILE_DIR/localhost.pem --key $ZSH_FILE_DIR/localhost.pem"
 alias https-server='http-server-ssl'
 
+alias pwgen='pwgen -scnyB1'
+
 # save pipe output to given variable e.g. echo foo | to bar; echo $bar 
 alias to='read -r'
 
