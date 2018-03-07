3faf869e16ec677d9b59004c414139b747a6c6bf
Bengt Brodersen <bengt.brodersen@gmail.com>
2018-03-07 22:59:35 +0100

    minor

--- app_configs/git/install.sh
+++ app_configs/git/install.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 SELF_DIR="$(dirname "$0")"
 cd "$SELF_DIR"
 
@@ -20,40 +20,40 @@ fi
 
 
 # list all alias
-git config --global alias.alias "\!git config --get-regexp alias | sort | sed -E -e 's|^alias\.||' | grep -v -e '^alias' | sort | sed -E -e 's|^([^ ]*)( .*)|${fg_bold[blue]}\1###${fg[white]}\2$reset_color|' | column -s '###' -t"
+git config --global alias.alias $'!git config --get-regexp alias | sort | sed -E -e \'s|^alias\.||\' | grep -v -e \'^alias\' | sort | sed -E -e "s|^([^ ]*)( .*)|$(printf \'\\e[34m\')\\1###$(printf \'\\e[39m\')\\2|" | column -s \'###\' -t'
 
 # amend to last commit
-git config --global alias.amend 'commit --amend --no-edit'
+git config --global alias.amend $'commit --amend --no-edit'
 
 # undo last commmit
-git config --global alias.retract 'reset HEAD^'
+git config --global alias.retract $'reset HEAD^'
 
 # reset to upstream state
-git config --global alias.rewind 'git reset --hard @{upstream}'
+git config --global alias.rewind $'reset --hard @{upstream}'
 
 # force push with lease
-git config --global alias.push-force 'push --force-with-lease'
+git config --global alias.push-force $'push --force-with-lease'
 
 # open ignore file with $EDITOR
-git config --global alias.ignore $'!sh -c "PATTERN=$1; if [ -n \'$PATTERN\' ]; then echo \'$PATTERN\' >> .gitignore; else $EDITOR .gitignore; fi; if [ -e .gitignore ]; then git add .gitignore; fi;"'
+git config --global alias.ignore $'!sh -c "PATTERN=$1; if [ -n \'$PATTERN\' ]; then echo \'$PATTERN\' >> .gitignore; else $EDITOR .gitignore; fi; if [ -e .gitignore ]; then git add .gitignore; fi"'
 
 # ignore changes of tracked file(s)
-git config --global alias.ignore-change 'update-index --skip-worktree'
+git config --global alias.ignore-change $'update-index --skip-worktree'
 
 # colorized log
-git config --global alias.graph $'!git log --color=always --graph --date=format:\'%a %Y-%m-%d %H:%M\' --pretty=tformat:\' %C(blue bold)%h%C(reset) %C(white bold)%s%C(reset) %C(dim white)%an%C(reset)%n ↪  %C(dim green)%ar%C(reset) %C(dim cyan)%ad%C(reset)%C(auto)%d%C(reset)\' -m'
+git config --global alias.graph $'log --color=always --graph --date=format:\'%a %Y-%m-%d %H:%M\' --pretty=tformat:\' %C(blue bold)%h%C(reset) %C(white bold)%s%C(reset) %C(dim white)%an%C(reset)%n ↪  %C(dim green)%ar%C(reset) %C(dim cyan)%ad%C(reset)%C(auto)%d%C(reset)\' -m'
 
 # get commit hash for HEAD by default or Branch or Tag
-git config --global alias.hash $'!sh -c "git rev-parse ${1:-HEAD}"'
+git config --global alias.hash $'!sh -c \'REV=$0; git rev-parse $REV\''
 
 # git init with empty root commit
 git config --global alias.bootstrap '!git init && git commit -m "root" --allow-empty'
 
 # short status  
-git config --global alias.situation 'status --short --branch'
+git config --global alias.situation $'status --short --branch'
 
 # execute for all git sub folders
-git config --global alias.workspace '!sh -c "CMD=$1; for repo in */; do ( cd \$repo && git status >/dev/null 2>&1 && printf \"\e[34m\${PWD#$PWD/}:\e[39m\n\" && git \$CMD && echo); true; done"'
+git config --global alias.workspace $'!sh -c \'WORKSPACE=$PWD; CMD="$0 $@"; for repo in */; do ( cd $repo && git status >/dev/null 2>&1 && printf "\\e[34m${PWD#$WORKSPACE/}:\\e[39m\\n" && git $CMD && echo); true; done\''
 
 
 git config --global merge.tool 'vimdiff'
