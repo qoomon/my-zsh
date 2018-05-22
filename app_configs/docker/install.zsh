e4b8c6d974689f864e617bf29900a97c76d27223
Bengt Brodersen <bengt.brodersen@gamil.com>
2018-05-22 13:04:15 +0200

    cleanup docker install script

--- app_configs/docker/install.zsh
+++ app_configs/docker/install.zsh
@@ -4,27 +4,15 @@ cd "$SELF_DIR"
 
 cp -i config.json $HOME/.docker/config.json
 
-# install zsh completions
-DOCKER_APP_COMPLETION_PATH='/Applications/Docker.app/Contents/Resources/etc'
-if [ -e "$DOCKER_APP_COMPLETION_PATH" ]; then
-  ZSH_SITE_FUNCTIONS_DIR='/usr/local/share/zsh/site-functions'
-  for comp_file in '/Applications/Docker.app/Contents/Resources/etc/'*'.zsh-completion'; do
-    comp_target_file="$comp_file"
-    comp_target_file="${comp_target_file##*/}"
-    comp_target_file="${comp_target_file%.zsh-completion}"
-    comp_target_file="$ZSH_SITE_FUNCTIONS_DIR/_${comp_target_file}"
-    if [ ! -e "$comp_target_file" ]; then
-      ln -s "$comp_file" "$comp_target_file"
-    fi
-  done
-fi
-
 if [ "$(uname)" = "Darwin" ]; then
   # workaround for slow docker compose
   grep -q -F 'localunixsocket' /etc/hosts || echo '127.0.0.1       localunixsocket' | sudo tee -a /etc/hosts
   
-  ln -s /usr/local/share/zsh/site-functions/_docker /Applications/Docker.app/Contents/Resources/etc/docker.zsh-completion
-  ln -s /usr/local/share/zsh/site-functions/_docker-compose /Applications/Docker.app/Contents/Resources/etc/docker-compose.zsh-completion
-  ln -s /usr/local/share/zsh/site-functions/_docker-machine /Applications/Docker.app/Contents/Resources/etc/docker-machine.zsh-completion
+  # install zsh completions
+  DOCKER_APP_COMPLETION_PATH='/Applications/Docker.app/Contents/Resources/etc'
+  ZSH_SITE_FUNCTIONS_DIR='/usr/local/share/zsh/site-functions'
+  [ -e "$DOCKER_APP_COMPLETION_PATH/docker.zsh-completion" ]         || ln -s "$ZSH_SITE_FUNCTIONS_DIR/_docker"         "$DOCKER_APP_COMPLETION_PATH/docker.zsh-completion"
+  [ -e "$DOCKER_APP_COMPLETION_PATH/docker-compose.zsh-completion" ] || ln -s "$ZSH_SITE_FUNCTIONS_DIR/_docker-compose" "$DOCKER_APP_COMPLETION_PATH/docker-compose.zsh-completion"
+  [ -e "$DOCKER_APP_COMPLETION_PATH/docker-machine.zsh-completion" ] || ln -s "$ZSH_SITE_FUNCTIONS_DIR/_docker-machine" "$DOCKER_APP_COMPLETION_PATH/docker-machine.zsh-completion"
 fi
 
