a92ebc1415c3ae4891f032982c9094a6b4db9751
Bengt Brodersen <bengt.brodersen@gamil.com>
2018-08-29 17:08:26 +0200

    add bat

--- app_configs/install_utils_macos.sh
+++ app_configs/install_utils_macos.sh
@@ -24,6 +24,8 @@ command -v brew >/dev/null || ruby -e "$(curl -fsSL https://raw.githubuserconten
 ! brew list zsh >/dev/null && brew install zsh
 ! brew list pwgen >/dev/null && brew install pwgen
 
+! brew list bat >/dev/null && brew install bat # cat with highlighing and git support; https://github.com/sharkdp/bat
+
 ! brew list vim >/dev/null && brew install vim
 ! brew list micro >/dev/null && brew install micro # https://github.com/zyedidia/micro
 ! brew list fzf >/dev/null && brew install fzf # https://github.com/junegunn/fzf # install shell extensions /usr/local/opt/fzf/install
