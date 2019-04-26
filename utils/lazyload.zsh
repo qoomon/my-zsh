439d4388a996353152004867be2718c43252e3d8
Bengt Brodersen <bengt.brodersen@gmail.com>
2019-04-26 15:35:51 +0200

    refactor: simplify lazyload

--- utils/lazyload.zsh
+++ utils/lazyload.zsh
@@ -3,19 +3,6 @@ function lazyload {
   local cmd_list=($@); shift $#
 
   for cmd_name in $cmd_list; do
-    alias $cmd_name="_lazyload '$load_cmd' '${(j:&:)cmd_list}' $cmd_name"
+    alias $cmd_name="unalias ${cmd_list}; $load_cmd; $cmd_name"
   done
-}
-
-function _lazyload {
-  local load_cmd=$1; shift
-  local load_aliases=(${(s:&:)1}); shift
-  local cmd=($@); shift $#
-
-  unalias $load_aliases
-  eval "$load_cmd"
-  if [[ ${functions[$load_cmd]} ]] && [[ $load_cmd = 'load:'* ]]; then
-    unfunction $load_cmd
-  fi
-  $cmd
 }
