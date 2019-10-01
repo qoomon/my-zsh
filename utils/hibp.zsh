aa4e7a5e9922a46f37e794e25622c7fd35196911
Bengt Brodersen <bengt.brodersen@gmail.com>
2019-10-01 17:29:37 +0200

    feat: add hibp clipperz checker

--- utils/hibp.zsh
+++ utils/hibp.zsh
@@ -1,10 +1,45 @@
+autoload +X -U colors && colors
+
 function hibp {
-  read -rs PASSWORD
-  PASSWORD_SHA1="$(echo -n $PASSWORD | shasum | cut -d' ' -f1 | tr a-z A-Z)"
-  if (curl -s "https://api.pwnedpasswords.com/range/${PASSWORD_SHA1:0:5}" | grep "${PASSWORD_SHA1:5}" >/dev/null)
-  then
-    echo 'Oh no — pwned!'
+  local password
+  if [[ -t 0 ]]; then
+    echo -n 'Password:'
+    read -s password
+    echo -en "\r\033[K"
   else
-    echo 'Good news — no pwnage found!'
+    read -s password
   fi
+  
+  local password_sha1="$(echo -n $password | shasum | cut -d' ' -f1 | tr a-z A-Z)"
+  if (curl -s "https://api.pwnedpasswords.com/range/${password_sha1:0:5}" \
+      | grep -q "${password_sha1:5}"); then
+    echo "${fg_bold[red]}Oh no — pwned!${reset_color}"
+    return 0
+  else
+    echo "${fg_bold[green]}Good news — no pwnage found!${reset_color}"
+    return 1
+  fi
+}
+
+function hibp_clipperz {
+  local clipperz_export_file="$1"
+  local card field secret
+  xmllint --xpath 'string(/html/body/div/div/textarea)' "$clipperz_export_file" \
+  | jq '.[] | . as $card | .currentVersion.fields[] | select(.hidden) | [$card.label,.label,.value] | @tsv' -r \
+  | grep -v 'ARCH' \
+  | while IFS=$'\t' read card field secret; do
+      if [[ "${field:l}" =~ (code|pin|cvc|cvv) ]] \
+          && [[ "${#secret}" -le 6 ]] \
+          && [[ "${#secret}" =~ ^[0-9]*$ ]] &&; then
+        continue
+      fi
+      
+      card="${card%% *}"
+      echo -n "${fg_bold[yellow]}Check...${reset_color}         =>    ${fg[cyan]}${card}${reset_color}   >   ${fg[blue]}${field}${reset_color}"
+      pwned='no' && echo -n "$secret" | hibp >/dev/null && pwned='yes'
+      echo -en "\r\033[K"
+      if [[ "$pwned" == 'yes' ]]; then
+        echo "${fg_bold[red]}Oh no — pwned!${reset_color}   =>    ${fg[cyan]}${card}${reset_color}   >   ${fg[blue]}${field}${reset_color}"
+      fi
+  done
 }
