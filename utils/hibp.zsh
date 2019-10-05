867d918ec98e33190978254c6e513c7b5123637c
Bengt Brodersen <bengt.brodersen@gmail.com>
2019-10-05 21:43:36 +0200

    feat: add have I been pwned for chrome password export

--- utils/hibp.zsh
+++ utils/hibp.zsh
@@ -9,9 +9,9 @@ function hibp {
   else
     read -s password
   fi
-  
+
   local password_sha1="$(echo -n $password | shasum | cut -d' ' -f1 | tr a-z A-Z)"
-  if (curl -s "https://api.pwnedpasswords.com/range/${password_sha1:0:5}" \
+  if (curl -Ns "https://api.pwnedpasswords.com/range/${password_sha1:0:5}" \
       | grep -q "${password_sha1:5}"); then
     echo "${fg_bold[red]}Oh no — pwned!${reset_color}"
     return 0
@@ -21,6 +21,8 @@ function hibp {
   fi
 }
 
+
+
 function hibp_clipperz {
   local clipperz_export_file="$1"
   local card field secret
@@ -28,17 +30,50 @@ function hibp_clipperz {
   | jq '.[] | . as $card | .currentVersion.fields[] | select(.hidden) | [$card.label,.label,.value] | @tsv' -r \
   | grep -v 'ARCH' \
   | while IFS=$'\t' read card field secret; do
+      if [[ "${secret}" == "" ]] ; then
+        continue
+      fi
+
       if [[ "${field:l}" =~ (code|pin|cvc|cvv) ]] \
           && [[ "${#secret}" =~ ^[0-9]{0,6}$ ]] &&; then
         continue
       fi
-      
-      card="${card%% *}"
-      echo -n "${fg_bold[yellow]}Check...${reset_color}         =>    ${fg[cyan]}${card}${reset_color}   >   ${fg[blue]}${field}${reset_color}"
-      pwned='no' && echo -n "$secret" | hibp >/dev/null && pwned='yes'
-      echo -en "\r\033[K"
-      if [[ "$pwned" == 'yes' ]]; then
-        echo "${fg_bold[red]}Oh no — pwned!${reset_color}   =>    ${fg[cyan]}${card}${reset_color}   >   ${fg[blue]}${field}${reset_color}"
+
+      (
+        card="${card%% *}"
+        echo -n "${fg_bold[yellow]}Check...${reset_color}         =>    ${fg[cyan]}${card}${reset_color}   >   ${fg[blue]}${field}${reset_color}"
+        pwned='no' && echo -n "$secret" | hibp >/dev/null && pwned='yes'
+        test $? -gt 128 && break
+        echo -en "\r\033[K"
+        if [[ "$pwned" == 'yes' ]]; then
+          echo "${fg_bold[red]}Oh no — pwned!${reset_color}   =>    ${fg[cyan]}${card}${reset_color}   >   ${fg[blue]}${field}${reset_color}"
+        fi
+      ) || break
+  done
+}
+
+
+
+function hibp_chrome {
+  local chrome_export_file="$1"
+  local card field secret
+  tail -n +2 "$chrome_export_file" \
+  | while IFS=$',' read name url username password; do
+      if [[ "${password}" == "" ]] ; then
+        continue
       fi
+
+      # if [[ "${#password}" =~ ^[0-9]{0,6}$ ]] &&; then
+      #   continue
+      # fi
+
+      (
+        echo -n "${fg_bold[yellow]}Check...${reset_color}         =>    ${fg[cyan]}${name}${reset_color}   >   ${fg[blue]}${username}${reset_color}"
+        pwned='no' && echo -n "$password" | hibp >/dev/null && pwned='yes'
+        echo -en "\r\033[K"
+        if [[ "$pwned" == 'yes' ]]; then
+          echo "${fg_bold[red]}Oh no — pwned!${reset_color}   =>    ${fg[cyan]}${name}(${url})${reset_color}   >   ${fg[blue]}${username}${reset_color}"
+        fi
+      ) || break
   done
 }
