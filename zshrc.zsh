360d36eff2d47e8a6ced1b2234520315b8b21773
Bengt Brodersen <bengt.brodersen@gmail.com>
2020-03-06 13:57:22 +0100

    fix: fix startup location

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,12 +1,11 @@
-cd $(dirname "$0")
-ZCONFIG_HOME=$PWD
+ZCONFIG_HOME=$(dirname $0)
 
 ### Add Commands Directory To Path
-path+="$ZCONFIG_HOME/commands"
+path=("$ZCONFIG_HOME/commands" $path)
 
-### modules
-source "modules/general.zsh"
-source "modules/history.zsh"
-source "modules/completion.zsh"
-source "modules/alias.zsh"
-source "modules/plugins.zsh"
+### Load Modules
+source "$ZCONFIG_HOME/modules/general.zsh"
+source "$ZCONFIG_HOME/modules/history.zsh"
+source "$ZCONFIG_HOME/modules/completion.zsh"
+source "$ZCONFIG_HOME/modules/alias.zsh"
+source "$ZCONFIG_HOME/modules/plugins.zsh"
