09a6352b6b90723a6fca97504065800751df504c
Bengt Brodersen <bengt.brodersen@gmail.com>
2020-03-16 08:53:57 +0100

    refactor: simplify zgem load snippet

--- modules/plugins.zsh
+++ modules/plugins.zsh
@@ -1,7 +1,7 @@
 ### load zgem plugin manager
-ZGEM_HOME="$HOME/.zgem"
+ZGEM_HOME="$HOME/.zsh.zgem"
 ZGEM_UTILS_DIR="$ZCONFIG_HOME/utils"
-if [[ ! -e "$ZGEM_HOME" ]] git clone 'https://github.com/qoomon/zgem.git' "$ZGEM_HOME"
+[[ -e "$ZGEM_HOME" ]] || git clone 'https://github.com/qoomon/zgem.git' "$ZGEM_HOME"
 source "$ZGEM_HOME/zgem.zsh" # && ZGEM_VERBOSE='true'
 
 ### Load Config Management 
