3710bafb07bf084eba5115f5a6a041e4bfa41011
Bengt Brodersen <bent.brodersen@gmail.com>
2021-06-28 14:34:00 +0200

    fix: fix jvm util command

--- utils/java.zsh
+++ utils/java.zsh
@@ -1,10 +1,10 @@
 function jvm {
   if [ "$1" = "-h" ]
   then
-    echo "jenv sets \$JAVA_HOME to a specific version"
+    echo "jvm sets \$JAVA_HOME to a specific version"
     echo "usage:"
-    echo "    jenv \t\tlist available versions"
-    echo "    jenv <version>\tsets \$JAVA_HOME accordingly to version"
+    echo "    jvm \t\tlist available versions"
+    echo "    jvm <version>\tsets \$JAVA_HOME accordingly to version"
     return 1
   fi
 
@@ -16,9 +16,9 @@ function jvm {
     echo 'Current Version'
     if [ "$JAVA_HOME" ]
     then
-      /usr/libexec/java_home -V 2>&1 | command grep "\t${JAVA_HOME}"
+      /usr/libexec/java_home -V 2>&1 | command grep "\s${JAVA_HOME}"
     else
-      /usr/libexec/java_home -V 2>&1 | command grep "\t$(/usr/libexec/java_home)"
+      /usr/libexec/java_home -V 2>&1 | command grep "\s$(/usr/libexec/java_home)"
     fi 
   else
     local java_home_path="$(/usr/libexec/java_home -v $1)"
