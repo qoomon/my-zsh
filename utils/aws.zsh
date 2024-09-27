7da5da627309640165755ce41ffe1131e0315624
Bengt Brodersen <bent.brodersen@gmail.com>
2024-09-27 09:54:25 +0200

    aws

--- utils/aws.zsh
+++ utils/aws.zsh
@@ -1,3 +1,14 @@
 function aws-export-credentials {
     eval "$(aws configure export-credentials --format env $@)"
 }
+
+compdef _curl curl-aws-sigv4
+function curl-aws-sigv4 {
+    aws-export-credentials --format env-no-export
+
+    curl \
+    --user "$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY" \
+    --header "x-amz-security-token: $AWS_SESSION_TOKEN" \
+    --aws-sigv4 "$@"
+}
+
